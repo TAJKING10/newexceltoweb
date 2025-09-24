ad4e76b6489b89481f1a6dac7ec4310ccdb38910
TOUFIC-JANDAH <toufic-jandah@hotmail.com>
2025-09-24 19:29:12 +0300

    oik+

--- payslip-web/src/components/admin/AdminCustomerPayslipManager.tsx
+++ payslip-web/src/components/admin/AdminCustomerPayslipManager.tsx
@@ -52,7 +52,9 @@ const Title = styled.h1`
 const Subtitle = styled.p`
   margin: 0;
   font-size: ${theme.typography.fontSize.lg};
-  opacity: 0.9;
+  color: white;
+  opacity: 0.95;
+  text-shadow: 0 1px 2px rgba(0,0,0,0.2);
 `;
 
 const MainContent = styled.div`
